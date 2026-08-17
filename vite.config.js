d3bbb92a976fa86926db61b404a1f93c53b89ba9
Zahra Moneeb <zahramoneeb@gmail.com>
2026-08-17 10:27:32 +0430

    Fix project setup and add portfolio sections

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 0fbd2a72a5f82fed8dde511705dbd22b6a4ad322
-})
+});
